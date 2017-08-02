a3718123febe5caf5aa9f8a8801aa94efbca10ed
Maxim Tkachenko <maxim@2dehands.be>
2017-08-02 21:22:26 +0200

    Project has mocked mode (to develop on emulators).

---
+++ app/src/mocked/java/org/weather/kweatherapp/LocationRepository.kt
@@ -0,0 +1,17 @@
+package org.weather.kweatherapp
+
+import android.location.Location
+import com.google.android.gms.location.FusedLocationProviderClient
+
+/**
+ * Created by mtkachenko on 26/07/17.
+ */
+class LocationRepository(private val locationClient: FusedLocationProviderClient) {
+
+    fun getCurrentLocation(callback: (Location?) -> Unit) {
+        val almere = Location("mocked")
+        almere.latitude = 52.371353
+        almere.longitude = 5.222124
+        callback(almere)
+    }
+}
